5f945bf6cd14b0d385f405a93ac5376fac4bd5c5
MukunthanPeriyasamy <mukunthanperiyasamy04@gmail.com>
2025-10-10 22:39:56 +0530

    changed the root of the Fast API

--- model/router.py
+++ model/router.py
@@ -1,12 +1,13 @@
 from fastapi import FastAPI , Body
+from fastapi.middleware.cors import CORSMiddleware
+from fastapi.responses import FileResponse
+from pathlib import Path
 from main import grammer_check_model , summarization_model
 
 app = FastAPI()
 @app.get("/")
 def root():
-    return {"message": "Welcome to the Grammar Correction AI. Use the /correct endpoint to submit text for grammar correction."}
-
-# GRAMMER CORRECTION ENDPOINT
+    return FileResponse(Path(__file__).parent.parent / "frontend" / "index.html")
 @app.post("/grammer_correction")
 def grammer_correction(request: str = Body()):
     corrected_text = grammer_check_model(request)
